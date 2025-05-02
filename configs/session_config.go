d511b78c57e5c12fe988783ad3764bf4df302c13
ZATRANO <36309658+zatrano@users.noreply.github.com>
2025-05-02 18:25:19 +0300

    Update session_config.go

--- configs/session_config.go
+++ configs/session_config.go
@@ -2,25 +2,13 @@ package configs
 
 import (
 	"encoding/gob"
-	"strconv"
 	"time"
 
 	"zatrano/models"
 	"zatrano/utils"
 
 	"github.com/gofiber/fiber/v2/middleware/session"
-	"github.com/gofiber/storage/postgres/v3"
-	"go.uber.org/zap"
 )
-
-type sessionDatabaseConfig struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	Name     string
-	SSLMode  string
-}
 
 var Session *session.Store
 
@@ -39,53 +27,17 @@ func SetupSession() *session.Store {
 }
 
 func createSessionStore() *session.Store {
-	portStr := utils.GetEnvWithDefault("DB_PORT", "5432")
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		utils.SLog.Fatalw("Invalid DB_PORT environment variable for session store",
-			"value", portStr,
-			"error", err,
-		)
-	}
-
-	dbConfig := sessionDatabaseConfig{
-		Host:     utils.GetEnvWithDefault("DB_HOST", "localhost"),
-		Port:     port,
-		User:     utils.GetEnvWithDefault("DB_USERNAME", "postgres"),
-		Password: utils.GetEnvWithDefault("DB_PASSWORD", ""),
-		Name:     utils.GetEnvWithDefault("DB_DATABASE", "myapp"),
-		SSLMode:  utils.GetEnvWithDefault("DB_SSL_MODE", "disable"),
-	}
-
-	utils.Log.Info("Configuring session storage",
-		zap.String("storage_type", "postgres"),
-		zap.String("host", dbConfig.Host),
-		zap.Int("port", dbConfig.Port),
-		zap.String("user", dbConfig.User),
-		zap.String("database", dbConfig.Name),
-		zap.String("sslmode", dbConfig.SSLMode),
-		zap.String("table", "sessions"),
-	)
-
-	storage := postgres.New(postgres.Config{
-		Host:       dbConfig.Host,
-		Port:       dbConfig.Port,
-		Username:   dbConfig.User,
-		Password:   dbConfig.Password,
-		Database:   dbConfig.Name,
-		SSLMode:    dbConfig.SSLMode,
-		Reset:      false,
-		Table:      "sessions",
-		GCInterval: 10 * time.Second,
-	})
-
 	sessionExpirationHours := utils.GetEnvAsInt("SESSION_EXPIRATION_HOURS", 24)
+
 	store := session.New(session.Config{
-		Storage:    storage,
-		Expiration: time.Duration(sessionExpirationHours) * time.Hour,
+		CookieHTTPOnly: false,
+		CookieSecure:   utils.GetEnvWithDefault("APP_ENV", "production"),
+		Expiration:     time.Duration(sessionExpirationHours) * time.Hour,
+		KeyLookup:      "cookie:session_id",
+		CookieSameSite: "Lax",
 	})
 
-	utils.SLog.Infof("Session store configured with %d hour expiration", sessionExpirationHours)
+	utils.SLog.Infof("Cookie-based session store configured with %d hour expiration", sessionExpirationHours)
 
 	registerGobTypes()
 
