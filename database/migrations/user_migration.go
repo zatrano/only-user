38127e1b8aeba7ffb57abcc5f0d0056ec42200a7
ZATRANO <36309658+zatrano@users.noreply.github.com>
2025-05-02 19:40:25 +0300

    Update user_migration.go

--- database/migrations/user_migration.go
+++ database/migrations/user_migration.go
@@ -1,6 +1,7 @@
 package migrations
 
 import (
+	"strings"
 	"zatrano/models"
 	"zatrano/utils"
 
@@ -9,18 +10,15 @@ import (
 )
 
 func MigrateUsersTable(db *gorm.DB) error {
-	err := db.Exec(`DO $$
-	BEGIN
-		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_type') THEN
-			CREATE TYPE user_type AS ENUM ('system', 'panel');
-		END IF;
-	END$$`).Error
-	if err != nil {
-		utils.Log.Error("Failed to create/check user_type enum", zap.Error(err))
+	// Enum tipini oluştur
+	err := db.Exec(`CREATE TYPE user_type AS ENUM ('system', 'panel')`).Error
+	if err != nil && !isAlreadyExistsError(err) {
+		utils.Log.Error("Failed to create user_type enum", zap.Error(err))
 		return err
 	}
 	utils.SLog.Debug("Checked/created user_type enum")
 
+	// Users tablosunu migrate et
 	err = db.AutoMigrate(&models.User{})
 	if err != nil {
 		utils.Log.Error("Failed to migrate users table structure", zap.Error(err))
@@ -29,4 +27,9 @@ func MigrateUsersTable(db *gorm.DB) error {
 	utils.SLog.Info("Users table structure migrated successfully")
 
 	return nil
+}
+
+// PostgreSQL enum tipi zaten varsa kontrol eder
+func isAlreadyExistsError(err error) bool {
+	return strings.Contains(err.Error(), "already exists")
 }
